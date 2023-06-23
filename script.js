b577272bb1300b57d2173db03e7465810072da68
Ajmal777 <81432526+Ajmal777@users.noreply.github.com>
2023-06-23 18:32:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 window.promises = [];
 
-for(let i=0; i<=5; i++){
+for(let i=0; i<5; i++){
 	promises.push(new Promise((res)=>{
-		const random = Math.floor(Math.rand()*5);
+		const random = Math.floor(Math.random()*5)+1;
 		setTimeout(()=>{
 			res(i);
 		}, random*1000);
-	})));
+	}));
 }
 const output = document.getElementById('output');
 Promise.any(promises)
